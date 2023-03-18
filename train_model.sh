d3740b3f14e0c0ce2be84792e3a56b50903f25c5
Roger <github@betechie.com>
2023-03-18 13:44:01 -0700

    apdate training script

--- train_model.sh
+++ train_model.sh
@@ -2,22 +2,22 @@
 
 torchrun --nproc_per_node=4 --master_port=9292 train.py \
     --model_name_or_path /src/weights/llama-7b \
-    --tokenizer_name_or_path /src/weights/tokenizer \
-    --data_path ./alpaca_data.json \
+    --tokenizer_name_or_path /home/ubuntu/weights/llama-13b \
+    --data_path /home/ubuntu/training-data \
     --bf16 True \
-    --output_dir alpaca_out \
+    --output_dir ~/alpaca_out \
     --num_train_epochs 3 \
     --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 8 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 2000 \
+    --save_steps 1000 \
     --learning_rate 2e-5 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
     --fsdp "full_shard auto_wrap" \
-    --fsdp_transformer_layer_cls_to_wrap 'LLaMADecoderLayer' \
-    --tf32 True \
+    --fsdp_transformer_layer_cls_to_wrap 'LlamaDecoderLayer' \
+    --tf32 True
